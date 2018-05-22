957d9b34b044544843ed4873493300c1af398d3e
Voimulax <voimulax@gmail.com>
2018-05-22 14:21:54 +0800

    frontend: [refactor] 建立学生模块的考试模块来管理考试相关的内容

--- frontend/src/app/student/student.module.ts
+++ frontend/src/app/student/student.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ExamDetailComponent } from './exam/exam-detail/exam-detail.component';
-import { ExamListComponent } from './exam/exam-list/exam-list.component';
-import { ExamService } from './exam/exam.service';
+import { ExamModule } from './exam/exam.module';
 import { StudentComponent } from './student/student.component';
 import { StudentRoutingModule } from './student-routing.module';
 
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  imports: [CommonModule, StudentRoutingModule, SharedModule],
-  providers: [ExamService],
-  declarations: [StudentComponent, ExamDetailComponent, ExamListComponent]
+  imports: [CommonModule, StudentRoutingModule, SharedModule, ExamModule],
+  declarations: [StudentComponent]
 })
 export class StudentModule {}
