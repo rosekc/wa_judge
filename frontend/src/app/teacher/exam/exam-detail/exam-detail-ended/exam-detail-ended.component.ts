1a40e797f9acb32dd750ae0d1e4de226fddae6ba
Voimulax <voimulax@gmail.com>
2018-05-27 16:27:49 +0800

    frontend: [feat] 教师模块考试详情已结束状态页面

--- frontend/src/app/teacher/exam/exam-detail/exam-detail-ended/exam-detail-ended.component.ts
+++ frontend/src/app/teacher/exam/exam-detail/exam-detail-ended/exam-detail-ended.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ExamInfo } from '../../exam-info.model';
+import { ExamService } from '../../exam.service';
 
 @Component({
   selector: 'app-exam-detail-ended',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exam-detail-ended.component.css']
 })
 export class ExamDetailEndedComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  get examInfo(): ExamInfo {
+    return this.examService.currentExamInfo;
   }
 
+  constructor(private examService: ExamService, private router: Router) {}
+
+  ngOnInit() {}
+
+  updateExamInfo() {
+    this.examService.updateCurrentExamInfo();
+  }
+
+  download() {}
+
+  goBack() {
+    this.router.navigate(['/teacher/exam']);
+  }
 }
